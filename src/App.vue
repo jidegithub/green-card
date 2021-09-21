45c2f8dce5693f5960d8171b52767f41ffffd23c
jide <jide.olutola@osisi.io>
2021-09-21 23:18:18 +0100

    update: chore

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="antialiased text-gray-900">
-    <div class="bg-gray-200 min-h-screen p-16 flex flex-col justify-center">
+    <div class="bg-gray-200 min-h-screen p-1">
       <Card :property="property"/>
     </div>
   </div>
@@ -22,9 +22,11 @@ export default {
       baths: 2,
       title: 'Modern home in city center',
       priceInCents: 190000,
-      formattedPrice: '$1,900.00',
-      reviewCount: 34,
-      rating: 4,
+      formattedPrice: '$750,000',
+      address: '742 Evergreen Terrace',
+      realtorImage: 'https://v1.tailwindcss.com/_next/static/media/erin-lindford.4804b52007ca82ebe9999d19c717b44d.jpg',
+      realtorName: 'tiffany heffner',
+      realtorPhone: '(555)555-4321',
     },
   }),
 };
@@ -32,11 +34,9 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,26 +1,39 @@
 <template>
-  <div>
+  <div class="max-w-sm m-auto">
     <div class="relative pb-5/6">
-      <img class="absolute h-full w-full object-cover rounded-lg shadow-md" :src="property.imageUrl" :alt="property.imageAlt">
+      <heart-icon class="absolute z-10 right-0 heart-icon"></heart-icon>
+      <img class="absolute h-full w-full object-cover rounded-t-xl shadow-md" :src='property.imageUrl' :alt='property.imageAlt'>
     </div>
-    <div class="relative px-4 -mt-16">
-      <div class="bg-white p-6 rounded-lg shadow-lg">
-        <div class="flex items-baseline">
-          <span class="inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide">New</span>
-          <div class="ml-2 text-gray-600 text-xs uppercase font-semibold tracking-wide">
-            {{ property.beds }} beds &bull; {{ property.baths }} baths
+    <div class="relative">
+      <div class="bg-white shadow-lg rounded-b-xl">
+        <div class="p-6">
+          <span class="inline-block text-sm uppercase text-gray-500 font-semibold tracking-wide">detached house &bull; 5y old</span>
+          <h4 class="mt-1 font-semibold text-2xl leading-tight truncate">{{ property.formattedPrice }}</h4>
+          <div class="mt-1 text-gray-600 bold">
+            {{ property.address }}
           </div>
         </div>
-        <h4 class="mt-1 font-semibold text-lg leading-tight truncate">{{ property.title }}</h4>
-        <div class="mt-1">
-          {{ property.formattedPrice }}
-          <span class="text-gray-600 text-sm"> / wk</span>
+        <div class="flex text-lg font-semibold tracking-wide border-t border-gray-100 px-6 py-5 capitalize">
+          <span class="flex amenities">
+            <bed-icon :iconName="bed"></bed-icon>
+            <h4 class="ml-2">{{ property.beds }} <span class="text-gray-400">bedrooms</span></h4>
+          </span>
+          <span class="flex">
+            <bath-icon :iconName="bath"></bath-icon>
+            <h4 class="ml-2">{{ property.baths }} <span class="text-gray-400">bathrooms</span></h4>
+          </span>
         </div>
-        <div class="mt-2 flex items-center">
-          <svg v-for="i in 5" :key="i" viewBox="0 0 24 24" :class="i <= property.rating ? 'text-teal-500' : 'text-gray-400'" class="h-4 w-4 fill-current">
-            <path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z" />
-          </svg>
-          <span class="ml-2 text-gray-600 text-sm">{{ property.reviewCount }} reviews</span>
+        <div class="bg-gray-100 border-t border-gray-100 h-full rounded-b-xl">
+          <div class="py-2 px-6 h-full">
+            <h2 class="uppercase font-semibold text-gray-400">Realtor</h2>
+            <div class="md:flex flex rounded-lg py-3">
+              <img class="h-16 w-16 rounded-full mr-6 md:mx-0 md:mr-6" :src='property.realtorImage' :alt='property.realtorName'>
+              <div class="md:text-left">
+                <h2 class="text-lg font-semibold capitalize">{{ property.realtorName }}</h2>
+                <div class="text-gray-600">{{ property.realtorPhone }}</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -28,7 +41,25 @@
 </template>
 
 <script>
+import BathIcon from './icons/IconBath.vue';
+import BedIcon from './icons/IconBed.vue';
+import HeartIcon from './icons/IconHeart.vue';
+
 export default {
   props: ['property'],
+  components: {
+    BathIcon,
+    BedIcon,
+    HeartIcon,
+  },
 };
 </script>
+<style scoped>
+  .amenities{
+    flex-grow: .4;
+  }
+  .heart-icon{
+    top: 12px;
+    right: 12px;
+  }
+</style>
